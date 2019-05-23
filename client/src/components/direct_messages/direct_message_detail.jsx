d20e329ab01c870ae6d74a476dec108218ef082a
valery-nguyen <valery.nguyen@ucla.edu>
2019-05-23 18:32:39 -0400

    fixed error when sending new dm

--- client/src/components/direct_messages/direct_message_detail.jsx
+++ client/src/components/direct_messages/direct_message_detail.jsx
@@ -21,8 +21,8 @@ class DirectMessageDetail extends React.Component {
       <Query query={FETCH_DIRECT_MESSAGES} variables={{ id: this.props.id }}>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>;
+          if (!data.directmessage) return null;
           if (error) return <p>Error</p>;
-          // console.log(data);
           return (
             <div className="channel-links-container">        
               <Link className="channel-link" to={`/dms/${data.directmessage._id}`}><h3 className="channel-name" >{`${data.directmessage.users[1].email}`}</h3></Link>
