38a98dc03b6f235844cc316612287b8a11c63cf2
Colin <colin.reitman87@gmail.com>
2019-05-23 20:49:36 -0400

    completed header console and adaptive names

--- client/src/components/direct_messages/dm_main.jsx
+++ client/src/components/direct_messages/dm_main.jsx
@@ -4,26 +4,37 @@ import Queries from "../../graphql/queries";
 import Mutations from "../../graphql/mutations";
 import Subscriptions from "../../graphql/subscriptions";
 import CreateDirectMessage from './../direct_messages/create_direct_message';
+import { HeaderConsole } from './../main_page/header_console';
 const { FETCH_DIRECT_MESSAGES } = Queries;
 const { NEW_DIRECT_MESSAGE_SUBSCRIPTION } = Subscriptions;
 
 class DMChat extends React.Component {
+
+  namesGetter(usersArray) {
+    let names = "";
+    usersArray.forEach((user) => {
+      if ( !names.includes(user.name) ) {
+        names += user.name;
+        names += ", ";
+      }
+    });
+    return names.slice(0, -2);
+  }
+
   render() {
     return (
       <Query query={FETCH_DIRECT_MESSAGES} variables={{ id: this.props.history.location.pathname.split("/").slice(-1)[0] }}>
         {({ loading, error, data}) => {
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
-<<<<<<< HEAD
-=======
           if (!data) return null;
->>>>>>> f7a2ecb53a105ef996de67db87d937f83d715986
           let allMessages;
           if (!data.directmessage) {
             allMessages = [];
           } else {
             allMessages = [].concat(data.directmessage.messages);
           }
+          let names = this.namesGetter(data.directmessage.users);
           let newData = data;
           let messageAuthor;
           return (
@@ -32,25 +43,28 @@ class DMChat extends React.Component {
             >
               {({ data, loading }) => {
                 return (
-                  <div className="main-chat-window">
-                    <ul className="message-list">
-                      {allMessages.map((message, idx) => {
-                        messageAuthor = newData.directmessage.users.filter((user) => (user._id === message.user_id))[0].name;
-                        return <li className="message-element" key={idx}>
-                        <div className="message-object">
-                        <img className="message-pic" src={require('./pika.jpg')} alt="pika"/>
-                          <div className="message-box">
-                            <div className="message-info">
-                                <p className="message-author">{messageAuthor}</p>
-                              <p className="message-date">{message.date}</p>
+                  <div>
+                    <HeaderConsole name={names}/>
+                    <div className="main-chat-window">
+                      <ul className="message-list">
+                        {allMessages.map((message, idx) => {
+                          messageAuthor = newData.directmessage.users.filter((user) => (user._id === message.user_id))[0].name;
+                          return <li className="message-element" key={idx}>
+                          <div className="message-object">
+                          <img className="message-pic" src={require('./pika.jpg')} alt="pika"/>
+                            <div className="message-box">
+                              <div className="message-info">
+                                  <p className="message-author">{messageAuthor}</p>
+                                <p className="message-date">{message.date}</p>
+                              </div>
+                              <p className="message-body">{message.body}</p>
                             </div>
-                            <p className="message-body">{message.body}</p>
                           </div>
-                        </div>
-                      </li>
-                      })}
-                    </ul>
-                    <CreateDirectMessage />
+                        </li>
+                        })}
+                      </ul>
+                      <CreateDirectMessage />
+                    </div>
                   </div>
                 );
               }}
